2cff92ca741245dba3f570d07dab4a38e041bb39
Viacheslav Ivanov <slava.eth@gmail.com>
2014-01-20 20:09:28 -0800

    refactored to use peewee orm

--- pindata.py
+++ pindata.py
@@ -1,121 +1,92 @@
-import sqlite3
+from peewee import *
 import os
 
-# If you intend to execute pindata.py from other paths be sure to provide the path below.
-# If you place pindata.py in /home/user/pipins then do mofiy the path accordingly.
-# Example change ('pindata.db') ==> ('/home/user/pipins/pindata.db')
+# path to you database, default is current folder
+DB_PATH = 'pindata.db'
 
-#------- CHANGE PATH HERE ---------
-db = sqlite3.connect ('pindata.db')
-#----------------------------------
-curs = db.cursor()
-x = curs.execute
-def createtable():
-    curs.execute ('CREATE TABLE pindata (id INTEGER PRIMARY KEY, pin INT, assign TEXT, intended TEXT)')
+db = SqliteDatabase( DB_PATH )
 
-def generatepins():
-    x ('DELETE FROM pindata')
-    x ('INSERT INTO pindata (pin) VALUES ("1")')
-    x ('INSERT INTO pindata (pin) VALUES ("2")')
-    x ('INSERT INTO pindata (pin) VALUES ("3")')
-    x ('INSERT INTO pindata (pin) VALUES ("4")')
-    x ('INSERT INTO pindata (pin) VALUES ("5")')
-    x ('INSERT INTO pindata (pin) VALUES ("6")')
-    x ('INSERT INTO pindata (pin) VALUES ("7")')
-    x ('INSERT INTO pindata (pin) VALUES ("8")')
-    x ('INSERT INTO pindata (pin) VALUES ("9")')
-    x ('INSERT INTO pindata (pin) VALUES ("10")')
-    x ('INSERT INTO pindata (pin) VALUES ("11")')
-    x ('INSERT INTO pindata (pin) VALUES ("12")')
-    x ('INSERT INTO pindata (pin) VALUES ("13")')
-    x ('INSERT INTO pindata (pin) VALUES ("14")')
-    x ('INSERT INTO pindata (pin) VALUES ("15")')
-    x ('INSERT INTO pindata (pin) VALUES ("16")')
-    x ('INSERT INTO pindata (pin) VALUES ("17")')
-    x ('INSERT INTO pindata (pin) VALUES ("18")')
-    x ('INSERT INTO pindata (pin) VALUES ("19")')
-    x ('INSERT INTO pindata (pin) VALUES ("20")')
-    x ('INSERT INTO pindata (pin) VALUES ("21")')
-    x ('INSERT INTO pindata (pin) VALUES ("22")')
-    x ('INSERT INTO pindata (pin) VALUES ("23")')
-    x ('INSERT INTO pindata (pin) VALUES ("24")')
-    x ('INSERT INTO pindata (pin) VALUES ("25")')
-    x ('INSERT INTO pindata (pin) VALUES ("26")')
-    x ('UPDATE pindata SET intended="+3.3v" WHERE pin="1"')
-    x ('UPDATE pindata SET intended="+5v" WHERE pin="2"')
-    x ('UPDATE pindata SET intended="GPIO 0" WHERE pin="3"')
-    x ('UPDATE pindata SET intended="+5V" WHERE pin="4"')
-    x ('UPDATE pindata SET intended="GPIO 1" WHERE pin="5"')
-    x ('UPDATE pindata SET intended="GND" WHERE pin="6"')
-    x ('UPDATE pindata SET intended="GPIO 4 1-wire" WHERE pin="7"')
-    x ('UPDATE pindata SET intended="GPIO 14 UART0_TXD" WHERE pin="8"')
-    x ('UPDATE pindata SET intended="GND" WHERE pin="9"')
-    x ('UPDATE pindata SET intended="GPIO 15 UART0_RXD" WHERE pin="10"')
-    x ('UPDATE pindata SET intended="GPIO 17" WHERE pin="11"')
-    x ('UPDATE pindata SET intended="GPIO 18 PCM_CLK" WHERE pin="12"')
-    x ('UPDATE pindata SET intended="GPIO 21" WHERE pin="13"')
-    x ('UPDATE pindata SET intended="GND" WHERE pin="14"')
-    x ('UPDATE pindata SET intended="GPIO 22" WHERE pin="15"')
-    x ('UPDATE pindata SET intended="GPIO 23" WHERE pin="16"')
-    x ('UPDATE pindata SET intended="GPIO +3.3v" WHERE pin="17"')
-    x ('UPDATE pindata SET intended="GPIO 24" WHERE pin="18"')
-    x ('UPDATE pindata SET intended="GPIO 10 SPI0_MOSI" WHERE pin="19"')
-    x ('UPDATE pindata SET intended="GND" WHERE pin="20"')
-    x ('UPDATE pindata SET intended="GPIO 9 SPI0_MISO" WHERE pin="21"')
-    x ('UPDATE pindata SET intended="GPIO 25" WHERE pin="22"')
-    x ('UPDATE pindata SET intended="GPIO 11 SPI0SCLK" WHERE pin="23"')
-    x ('UPDATE pindata SET intended="GPIO 8 SPI_CE0_N" WHERE pin="24"')
-    x ('UPDATE pindata SET intended="GND" WHERE pin="25"')
-    x ('UPDATE pindata SET intended="GPIO 7 SPI0_CE1_N" WHERE pin="26"')
-    db.commit()
+class BaseModel( Model ):
+  """Base model class for this application that all model will extend from"""
+  class Meta:
+    database = db
 
-try:
-    createtable()
-except:
-    print ("Already Created")
+class Pin( BaseModel ):
+  """Pin model"""
+  number      = IntegerField()
+  about       = CharField()
+  assigned    = BooleanField()
+  description = CharField( null=True )
 
-check = 'SELECT COUNT(id) FROM pindata'
-query = x (check)
-result = query.fetchone()
-check = result[0]
-if check < 1:
-    generatepins()
+  def reset( self ):
+    self.assigned = False;
+    description = None;
+    self.save()
 
+  @staticmethod
+  def populate():
+    Pin.create( number=1,   assigned=False,  about='+3.3v' )
+    Pin.create( number=2,   assigned=False,  about='+5v' )
+    Pin.create( number=3,   assigned=False,  about='GPIO 0' )
+    Pin.create( number=4,   assigned=False,  about='+5v' )
+    Pin.create( number=5,   assigned=False,  about='GPIO 1' )
+    Pin.create( number=6,   assigned=False,  about='GND' )
+    Pin.create( number=7,   assigned=False,  about='GPIO 4 1-wire' )
+    Pin.create( number=8,   assigned=False,  about='GPIO 14 UART0_TXD' )
+    Pin.create( number=9,   assigned=False,  about='GND' )
+    Pin.create( number=10,  assigned=False,  about='GPIO 15 UART0_RXD' )
+    Pin.create( number=11,  assigned=False,  about='GPIO 17' )
+    Pin.create( number=12,  assigned=False,  about='GPIO 18 PCM_CLK' )
+    Pin.create( number=13,  assigned=False,  about='GPIO 21' )
+    Pin.create( number=14,  assigned=False,  about='GND' )
+    Pin.create( number=15,  assigned=False,  about='GPIO 22' )
+    Pin.create( number=16,  assigned=False,  about='GPIO 23' )
+    Pin.create( number=17,  assigned=False,  about='GPIO +3.3v' )
+    Pin.create( number=18,  assigned=False,  about='GPIO 24' )
+    Pin.create( number=19,  assigned=False,  about='GPIO 10 SPI0_MOSI' )
+    Pin.create( number=20,  assigned=False,  about='GND' )
+    Pin.create( number=21,  assigned=False,  about='GPIO 9 SPI0_MISO' )
+    Pin.create( number=22,  assigned=False,  about='GPIO 25' )
+    Pin.create( number=23,  assigned=False,  about='GPIO 11 SPI0SCLK' )
+    Pin.create( number=24,  assigned=False,  about='GPIO 8 SPI_CE0_N' )
+    Pin.create( number=25,  assigned=False,  about='GND' )
+    Pin.create( number=26,  assigned=False,  about='GPIO 7 SPI0_CE1_N' )
 
+# connect to our database
+db.connect()
 
+# create the tables and populate with defalut data
+if not Pin.table_exists():
+  Pin.create_table()
+  Pin.populate()
 
-true = 1
-while true:
-    os.system("clear")
- 
-    x ('select * from pindata')
-    result = curs.fetchall()
-    print '%-5s %-30s %-20s' % ("Pin","Description","Default")
-    print '-----------------------------------------------------------'
-    for i in result:
-        print '%-5s %-30s %-20s %1s' % (i[1],i[2],i[3],i[1])
-    print ("")
-    result = x ('SELECT COUNT(assign) FROM pindata')
-    result = curs.fetchone()
-    print result[0], ("Pin(s) currently in use")
-    print ("")
-    print ("type in (exit) to quit.")
-    selectpin = raw_input("Modify Pin:")
-    if selectpin == ("exit"):
-        exit()
-    print ("(update) or (delete)")
-    do = raw_input("Do:")
-    if do == ("update"):
-        pindetail = raw_input("Description:")
-        result = x ('UPDATE pindata SET assign=? WHERE pin=?', [pindetail, selectpin])
-        db.commit()
+while True:
+  os.system( 'clear' )
+  pins = Pin.select()
 
-    if do == ("delete"):
-        result = x ('UPDATE pindata SET assign=NULL WHERE pin=?', [selectpin])
-        db.commit()
+  # header
+  print '%-5s %-25s %-10s %-30s' % ( 'Pin', 'About', 'Assigned?', 'Description' )
+  print '-----------------------------------------------------------'
+  for pin in pins:
+    print '%-5s %-25s %-10s %-30s' % ( pin.number, pin.about, 'Y' if pin.assigned else 'N', pin.description)
 
-    if do == ("exit"):
-        exit()
+  number_of_used_pins = pins.where( Pin.assigned == True ).count()
+  print 'Pin(s) currently in use: %d' % number_of_used_pins
+
+  pin_number = int( raw_input( 'Modify Pin: ') )
+  selected_pin = Pin.get( Pin.number == pin_number )
+
+  command = raw_input( '>>  ' )
+  if command == 'update':
+    selected_pin.description = raw_input( 'Description: ')
+    selected_pin.assigned = True
+    selected_pin.save()
+
+  if command == 'reset':
+    selected_pin.reset();
+
+  if command == 'exit':
+    break;
 
 
 
